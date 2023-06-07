844684e2404789d5f1f532c822f4c862dadfa800
10560 <wren.moyer@protonmail.com>
2023-06-07 17:29:11 -0400

    started adding documentation :)

--- src/main/java/frc/robot/subsystems/Arms.java
+++ src/main/java/frc/robot/subsystems/Arms.java
@@ -1,7 +1,4 @@
-//toggles!!!
-
-//done 
-
+// documented
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
@@ -11,29 +8,37 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arms extends SubsystemBase {
 
+  // make variables for left and right arm motors
   private final CANSparkMax leftArm = new CANSparkMax(6, MotorType.kBrushless);
   private final CANSparkMax rightArm = new CANSparkMax(7, MotorType.kBrushless);
 
+  // groups arm motors so that they move in the same way at the same time
   private final MotorControllerGroup armS = new MotorControllerGroup(leftArm, rightArm);
 
+  // empty method, i dont think its important
   public Arms() {
 
   }
 
+  // method that sets the motor output to -0.8, so the motors unwind the string
   public void armOutStart() {
     armS.set(-0.8);
   }
 
+  // sets the motor output to 0, stopping it 
   public void armStop() {
     armS.set(0);
   }
 
+  // method that sets the motor output to 0.5, so the motors rewind the string 
   public void armInStart() {
     armS.set(0.5);
   }
 
   @Override
   public void periodic() {
+    // the left arm motor moved in the opposite way from the right arm motor, so 
+    // we had to invert one of them to make them match
     leftArm.setInverted(true);
   }
 
--- src/main/java/frc/robot/subsystems/IntakeTransport.java
+++ src/main/java/frc/robot/subsystems/IntakeTransport.java
@@ -1,4 +1,4 @@
-
+// documented
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
@@ -11,6 +11,11 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeTransport extends SubsystemBase {
 
+  // 15+16 were for the spinny bars at the bottom of the intake 
+  // 19+20 grouped these motors so they would move in the same way at the same time
+  // 17+20+21 are commented because someebody forgot to check the weight limit so 
+  // we had to remove the second intake :(
+  
   // private final Spark intakeFront = new Spark(0);
   private final Spark intakeBack = new Spark(0);
 
@@ -19,26 +24,36 @@ public class IntakeTransport extends SubsystemBase {
 
   private final Spark horizTransport = new Spark(4);
 
+  // this is for the pcm, which controls all solenoid movement (pneumatics equivalent of a motorcontroller)
   private PneumaticsControlModule pCM;
 
+  // solenoids for extending/retracting the front+back intakes and the top flap
+  // (top flap got torn off too, it never worked idk why)
   private final DoubleSolenoid frontSolenoid;
   private final DoubleSolenoid backSolenoid;
   private final DoubleSolenoid topSolenoid;
 
   public IntakeTransport(PneumaticsControlModule pCM) {
+
+    // java things- "the pcm in this method is the same pcm as the other one"
     this.pCM = pCM;
+
+    // these establish that the solenoids are controlled by the pcm, and which way is up/down
+    // the numbers coincide with what port the solenoids are plugged into
     backSolenoid = pCM.makeDoubleSolenoid(1, 0); // down/foward, up/reverse
     frontSolenoid = pCM.makeDoubleSolenoid(2, 3);
     topSolenoid = pCM.makeDoubleSolenoid(5, 4);
 
+    // these set the default values/starting position
     backSolenoid.set(Value.kReverse);
     frontSolenoid.set(Value.kReverse);
     topSolenoid.set(Value.kForward);
 
   }
+ 
+  // HOUSED IN INTAKE TRANSPORT (methods below were called in intakestransport command)
 
-  // HOUSED IN INTAKE TRANSPORT
-
+  // methods for the old front intake </3 rip front intake
   /*
    * public void wristDownFront() {
    * frontSolenoid.set(Value.kForward);
@@ -53,27 +68,34 @@ public class IntakeTransport extends SubsystemBase {
    * }
    */
 
+  // activates solenoid (intake goes down), sets roller output to 1 (roller spins, pulling in the ball),
+  // and sets horizontal transport output to 0.5 (internal wheels pull in the ball)
   public void wristDownBack() {
     frontSolenoid.set(Value.kForward);
     intakeBack.set(1);
     horizTransport.set(0.5);
   }
 
+  // just sets the horizontal transport output to 0.5
   public void horizTransportOn() {
     horizTransport.set(0.5);
   }
 
+  // turns of horizontal transport motor
   public void horizTransportOff() {
     horizTransport.set(0);
   }
 
+  // pulls the intake back up, turns off all intake motors
   public void wristUpBack() {
     frontSolenoid.set(Value.kReverse);
     intakeBack.set(0);
     horizTransport.set(0);
   }
 
-  // HOUSED IN PNEUMATICS
+  // HOUSED IN PNEUMATICS (methods below are called in the pneumatics command)
+
+  // these actually dont do anything because there is no top solenoid anymore rip
   public void topSolenoidDown() {
     topSolenoid.set(Value.kForward);
   }
